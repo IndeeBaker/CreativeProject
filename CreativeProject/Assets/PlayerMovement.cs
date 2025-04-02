fa2951aec5233da08dae9be68f18c09ec70d8bf5
IndeeBaker <IndeeBaker@gmail.com>
2025-04-02 18:33:22 +0100

    Added Menu and Map with New Input system

--- CreativeProject/Assets/PlayerMovement.cs
+++ CreativeProject/Assets/PlayerMovement.cs
@@ -1,15 +1,20 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float movementSpeed = 0.5f; //controls how fast the player moves
     public float cameraSmoothFollowSpeed;
+    public GameObject menu, map;
+    private InputSystem_Actions inputActions;
     // Update is called once per frame
 
     private void Awake()
     {
-       // controls = new InputActions;
+        inputActions = new InputSystem_Actions(); // Initialize the input actions
+        inputActions.UI.Menu.performed += ctx => ToggleMenu();
+        inputActions.UI.Map.performed += ctx => ToggleMap();
     }
 
     void Update()
@@ -23,10 +28,24 @@ public class PlayerMovement : MonoBehaviour
         Vector3 camPos = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z); //follows player with a delay (lerp)
         Vector3 playerPos = new Vector3(transform.position.x, transform.position.y, -10f);
         Camera.main.transform.position = Vector3.Lerp(camPos, playerPos, Time.deltaTime * cameraSmoothFollowSpeed);
+    }
 
+    private void OnEnable()
+    {
+        inputActions.Enable(); // Enable the input actions when the key is enabled
+    }
 
+    private void OnDisable()
+    {
+        inputActions.Disable(); // Disable the input actions when the key is disabled
+    }
+    private void ToggleMenu()
+    {
+        menu.SetActive(!menu.activeSelf); //toggles menu game object
+    }
 
+    private void ToggleMap()
+    {
+        map.SetActive(!map.activeSelf); //toggles map
     }
 }
-
-//This code script was double-checked with Co-Pilot
