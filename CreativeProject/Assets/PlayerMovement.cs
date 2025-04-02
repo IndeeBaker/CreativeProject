2ceb20b7106734bfbf230445c3a1ea1b74151e18
IndeeBaker <IndeeBaker@gmail.com>
2025-04-02 18:03:27 +0100

    Re-saved Movement after Unity Crashed

--- CreativeProject/Assets/PlayerMovement.cs
+++ CreativeProject/Assets/PlayerMovement.cs
@@ -9,7 +9,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void Awake()
     {
-        controls = new InputActions;
+       // controls = new InputActions;
     }
 
     void Update()
