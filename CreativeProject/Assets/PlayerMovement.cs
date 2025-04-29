d38219b2854c6e57708fd8d2b03139b144ad2285
oxxeys <IndeeBaker@gmail.com>
2025-04-29 11:24:55 +0100

    Added in Assets and Namespace
    Added in a namespace on the playerMovement script as there was a fatal error, added in assets

--- CreativeProject/Assets/PlayerMovement.cs
+++ CreativeProject/Assets/PlayerMovement.cs
@@ -2,50 +2,53 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour
+namespace playerMovement
 {
-    public float movementSpeed = 0.5f; //controls how fast the player moves
-    public float cameraSmoothFollowSpeed;
-    public GameObject menu, map;
-    private InputSystem_Actions inputActions;
-    // Update is called once per frame
-
-    private void Awake()
+    public class PlayerMovement : MonoBehaviour
     {
-        inputActions = new InputSystem_Actions(); // Initialize the input actions
-        inputActions.UI.Menu.performed += ctx => ToggleMenu();
-        inputActions.UI.Map.performed += ctx => ToggleMap();
-    }
+        public float movementSpeed = 0.5f; //controls how fast the player moves
+        public float cameraSmoothFollowSpeed;
+        public GameObject menu, map;
+        private InputSystem_Actions inputActions;
+        // Update is called once per frame
 
-    void Update()
-    {
-        float moveX = Input.GetAxis("Horizontal"); //A/D arrows
-        float moveY = Input.GetAxis("Vertical"); //W/S arrows
+        private void Awake()
+        {
+            inputActions = new InputSystem_Actions(); // Initialize the input actions
+            inputActions.UI.Menu.performed += ctx => ToggleMenu();
+            inputActions.UI.Map.performed += ctx => ToggleMap();
+        }
 
-        Vector2 movement = new Vector2(moveX, moveY); //vector2 combines both inputs into one single vector 
+        void Update()
+        {
+            float moveX = Input.GetAxis("Horizontal"); //A/D arrows
+            float moveY = Input.GetAxis("Vertical"); //W/S arrows
 
-        transform.Translate(movement * movementSpeed * Time.deltaTime); //move cgaracter based on both inputs and the speed
-        Vector3 camPos = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z); //follows player with a delay (lerp)
-        Vector3 playerPos = new Vector3(transform.position.x, transform.position.y, -10f);
-        Camera.main.transform.position = Vector3.Lerp(camPos, playerPos, Time.deltaTime * cameraSmoothFollowSpeed);
-    }
+            Vector2 movement = new Vector2(moveX, moveY); //vector2 combines both inputs into one single vector 
 
-    private void OnEnable()
-    {
-        inputActions.Enable(); // Enable the input actions when the key is enabled
-    }
+            transform.Translate(movement * movementSpeed * Time.deltaTime); //move cgaracter based on both inputs and the speed
+            Vector3 camPos = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z); //follows player with a delay (lerp)
+            Vector3 playerPos = new Vector3(transform.position.x, transform.position.y, -10f);
+            Camera.main.transform.position = Vector3.Lerp(camPos, playerPos, Time.deltaTime * cameraSmoothFollowSpeed);
+        }
 
-    private void OnDisable()
-    {
-        inputActions.Disable(); // Disable the input actions when the key is disabled
-    }
-    private void ToggleMenu()
-    {
-        menu.SetActive(!menu.activeSelf); //toggles menu game object
-    }
+        private void OnEnable()
+        {
+            inputActions.Enable(); // Enable the input actions when the key is enabled
+        }
 
-    private void ToggleMap()
-    {
-        map.SetActive(!map.activeSelf); //toggles map
+        private void OnDisable()
+        {
+            inputActions.Disable(); // Disable the input actions when the key is disabled
+        }
+        private void ToggleMenu()
+        {
+            menu.SetActive(!menu.activeSelf); //toggles menu game object
+        }
+
+        private void ToggleMap()
+        {
+            map.SetActive(!map.activeSelf); //toggles map
+        }
     }
 }
