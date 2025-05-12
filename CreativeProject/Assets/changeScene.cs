7b2dcffd158e5b4bfa008b8b8e3ba2aa22b164cd
IndeeBaker <IndeeBaker@gmail.com>
2025-05-12 16:59:36 +0100

    Animation Started

--- CreativeProject/Assets/changeScene.cs
+++ CreativeProject/Assets/changeScene.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class changeScene : MonoBehaviour
 {
+    public BoxCollider2D player;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+       
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision == player)
+        {
+            Debug.Log("Entered");
+            SceneManager.LoadScene(sceneName: "SampleScene");
+        }
     }
+
 }
